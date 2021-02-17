a4f7e0450214fa62367bcd7082739e1048fcc8f5
Santiago Rincon <santiagorincon2001@gmail.com>
2021-02-17 07:25:20 -0500

    Se coneca redux y react
    Ademas se agrega paquete para visualizar el store en el navegador

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import store from './redux/store';
 import './App.css';
 import Personas from "./components/Personas"
+import {Provider} from "react-redux"
 function App() {
   console.log(store.getState())
   return (
-    <div className="App">
-      <div>
-      Cantidad personas: 30
-      <Personas personas={[{nombre:"carlos",apellido:"juan",edad:15},]}></Personas>
-      </div> 
-      <div>
-      Cantidad productos: 20
+    <Provider store={store}>
+      <div className="App">
+            <div>
+            Cantidad personas: 30
+            <Personas personas={[{nombre:"carlos",apellido:"juan",edad:15},]}></Personas>
+            </div> 
+            <div>
+            Cantidad productos: 20
+            </div>
       </div>
-    </div>
+    </Provider>
+    
   );
 }
 
